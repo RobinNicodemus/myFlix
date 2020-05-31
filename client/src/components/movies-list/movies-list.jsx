45f2c590ccb67060fc213cf50b9ecfa80a201b3f
Robin <nicodemusnepomuk@gmail.com>
2020-05-31 03:16:42 +0200

    rebuild

--- client/src/components/movies-list/movies-list.jsx
+++ client/src/components/movies-list/movies-list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Row from 'react-bootstrap/Row';
 import { MovieCard } from '../movie-card/movie-card';
